957287d620a127a0568e4fd30f54b7f437fe282e
ducdat <ducdat.kenn01@gmail.com>
2025-09-16 20:12:34 +0700

    Bài 1 TS

---
+++ script.js
@@ -0,0 +1,4 @@
+let name = "ducdat"
+let age = "21"
+age = "36"
+console.log(age);
---
+++ script.ts
@@ -0,0 +1,8 @@
+function chuViVaDienTich(dai: number, rong:number){
+    const chuVi = (dai + rong) * 2;
+    const dienTich = dai * rong;
+
+    console.log("Chu vi:",chuVi);
+    console.log("Dien tich:",dienTich);
+}
+chuViVaDienTich(10, 5);
